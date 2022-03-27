90b4e4b97281b6b3dabcb25c2fde83967b40ed52
mohammad <mr.dori.dev@gmail.com>
2022-03-27 19:16:25 +0430

    add auto language recognition

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 """core URL Configuration
 """
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user_profile.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('user_profile.urls')),
+)
