062c2c75c424243e60e0b94f5558d389a956cd9b
mohammad <mr.dori.dev@gmail.com>
2022-03-27 18:07:28 +0430

    add locale path to settings.py

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,6 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# file for translations
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
