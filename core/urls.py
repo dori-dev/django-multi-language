2ed2d258bb19fe2c6a9dbf872168703f47594891
mohammad <mr.dori.dev@gmail.com>
2022-03-27 18:59:10 +0430

    fix problems

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user_profile'))
+    path('', include('user_profile.urls')),
 ]
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,7 @@
 """user profile urls
 """
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.IndexPage.as_view(), name='index')
